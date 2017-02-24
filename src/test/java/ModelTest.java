362520f27b6d4b01987b00e5926037d223a5c730
Kanter <Kanter>
2017-02-24 12:27:24 +0000

    Updates JUnit test cases and added model for test testing

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -1,50 +1,67 @@
 import static org.junit.Assert.*;
-
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import org.junit.Test;
 
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ModelTest {
+	
+	/**
+	 * Adam Kantorik, 24/02/2017
+	 */
+	
+	/**
+	 * Needs JUnitTest 4.11 or higher - ordering of tests.
+	 * Changed test to load data just once. A at the beginning of loading test makes sure
+	 * loading tests will run first. I also corrected test format.
+	 */
 
-	/**
-	 * Sam Law, 22/02/2017
-	 */
+	Model myModel = new Model();
 	
-	/**
-	 * For testing, I've assumed we can load a test .csv file into the model
-	 * for which we already know the various values, and test it to see if it
-	 * outputs the same values. The test .csv file is just the data from the
-	 * first day from the files available on the COMP2211 Notes page
-	 * 
-	 * Tell me how to correct if wrong!
-	 */
+	@Test
+	public void AloadImpressionLogTest(){
+		assertTrue(myModel.loadImpressionLog("test_impression_log.csv"));
+	}
 	
-//	public void totalImpressionsTest() {
-//		Model myModel = new Model("test_impression_log.csv");
-//		assertEquals("Correct total number of impressions:", 22049, myModel.totalImpressions());
-//	}
-//
-//	public void costPerClickTest() {
-//		Model myModel = new Model("test_click_log.csv");
-//		assertEquals("Correct cost per click:", 4.85, myModel.costPerClick());
-//	}
-//
-//	public void totalClicksTest() {
-//		Model myModel = new Model("test_click_log.csv");
-//		assertEquals("Correct total number of clicks:", 1079, myModel.totalClicks());
-//	}
-//
-//	public void clickThroughRateTest() {
-//		Model myModel = new Model("test_impression_log.csv", "test_impression_log.csv");
-//		assertEquals("Correct click-through-rate:", 0.0489, myModel.clickThroughRate());
-//	}
-//
-//	public void totalConversionsTest() {
-//		Model myModel = new Model("test_server_log.csv");
-//		assertEquals("Correct total number of conversions:", 95, myModel.totalConverions());
-//	}
-//
-//	public void totalCostTest() {
-//		Model myModel = new Model("test_impression_log.csv", "test_impression_log.csv");
-//		assertEquals("Correct total cost:", 5250.71, myModel.totalCost());
-//	}
+	@Test
+	public void AloadClickLogTest(){
+		assertTrue(myModel.loadClickLog("test_click_log.csv"));
+	}
+	
+	@Test
+	public void AloadServerLog(){
+		assertTrue(myModel.loadServerLog("test_click_log.csv"));
+	}
+	
+	@Test
+	public void totalImpressionsTest() {
+		assertEquals("Correct total number of impressions:", 22049, myModel.totalImpressions());
+	}
+	
+	@Test
+	public void costPerClickTest() {
+		assertEquals("Correct cost per click:", 4.85, myModel.costPerClick());
+	}
+	
+	@Test
+	public void totalClicksTest() {
+		assertEquals("Correct total number of clicks:", 1079, myModel.totalClicks());
+	}
+	
+	@Test
+	public void clickThroughRateTest() {
+		assertEquals("Correct click-through-rate:", 0.0489, myModel.clickThroughRate());
+	}
+	
+	@Test
+	public void totalConversionsTest() {
+		assertEquals("Correct total number of conversions:", 95, myModel.totalConversions());
+	}
+	
+	@Test
+	public void totalCostTest() {
+		assertEquals("Correct total cost:", 5250.71, myModel.totalCost());
+	}
+
 }
