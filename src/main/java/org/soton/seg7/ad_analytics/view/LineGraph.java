58b501788571081ff27251b58f71dc32f6a1593d
Bogdan Buduroiu <bbuduroiu@gmail.com>
2017-02-26 13:17:42 +0000

    Moved view files in the sources folder
    Please use /src/main/java/org/.../view for view files

---
+++ src/main/java/org/soton/seg7/ad_analytics/view/LineGraph.java
@@ -0,0 +1,20 @@
+package controller.model;
+
+import javafx.collections.ObservableList;
+import javafx.scene.chart.LineChart;
+
+public class LineGraph extends Graph{
+	
+	public LineGraph(String graphTitle){
+		super(graphTitle);
+		
+        
+        
+	}
+
+	@Override
+	public ObservableList getData() {
+		return null;
+		
+	}
+}
