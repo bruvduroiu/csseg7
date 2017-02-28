67730babf3ede9317c1cee6e3ff4c2626c578bb7
Bogdan Buduroiu <bbuduroiu@gmail.com>
2017-02-28 16:26:56 +0000

    Changed Tableview to ListView and other changes

--- src/main/java/org/soton/seg7/ad_analytics/controller/OverviewController.java
+++ src/main/java/org/soton/seg7/ad_analytics/controller/OverviewController.java
@@ -1,47 +1,86 @@
 package org.soton.seg7.ad_analytics.controller;
-
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.chart.LineChart;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import org.soton.seg7.ad_analytics.view.Graph;
+import javafx.scene.control.ListView;
 import org.soton.seg7.ad_analytics.view.MainView;
-
 public class OverviewController {
-
     @FXML
-    private TableView<Graph> graphTable;
-    @FXML
-    private TableColumn<Graph, String> graphTitleColumn;
+    private ListView<String> graphList;
     @FXML
     private Label graphTitleLabel;
     @FXML
-    private LineChart lineChart;
+    private ObservableList<String> list;
 
     // Reference to the main application.
     private MainView mainView;
-
     public OverviewController() {
     }
-    
+
     @FXML
     private void initialize() {
-        graphTitleColumn.setCellValueFactory(cellData -> cellData.getValue().graphTitleProperty());
-        showGraphDetails(null);
+        list = graphList.getItems();
+        list.add("Cost per Click");
+        list.add("Number of Impressions");
+        list.add("Number of Clicks");
+        list.add("Click through Rate");
+        list.add("Number of Conversions");
+        list.add("Total Cost");
+
+
         // Listen for selection changes and show the person details when changed.
-        graphTable.getSelectionModel().selectedItemProperty().addListener(
-                (observable, oldValue, newValue) -> showGraphDetails(newValue));
-    }
+        graphList.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> loadGraph(newValue));
 
-    private void showGraphDetails(Graph graph) {
-    	if( graph != null){
-    		graphTitleLabel.setText(graph.getGraphTitle());
-    	}
     }
+    private void loadGraph(String graph){
+        switch (graph) {
+            case "Cost per Click":
+                loadCostPerClick();
+                break;
+            case "Number of Imrpessions":
+                loadNumberOfImpressions();
+                break;
+            case "Number of Clicks":
+                loadNumberOfClicks();
+                break;
+            case "Click through Rate":
+                loadClickThroughRate();
+                break;
+            case "NumberOfConversions":
+                loadNumberOfConversions();
+                break;
+            case "Total Cost":
+                loadTotalCost();
+                break;
+        }
+    }
+    private void loadTotalCost() {
+        // TODO Auto-generated method stub
 
+    }
+    private void loadNumberOfConversions() {
+        // TODO Auto-generated method stub
+
+    }
+    private void loadClickThroughRate() {
+        // TODO Auto-generated method stub
+
+    }
+    private void loadNumberOfClicks() {
+        // TODO Auto-generated method stub
+
+    }
+    private void loadNumberOfImpressions() {
+        // TODO Auto-generated method stub
+
+    }
+    private void loadCostPerClick() {
+        // TODO Auto-generated method stub
+
+    }
     public void setMainView(MainView mainView) {
         this.mainView = mainView;
-        graphTable.setItems(mainView.getGraphData());
+
     }
 }
--- src/main/java/org/soton/seg7/ad_analytics/view/MainView.java
+++ src/main/java/org/soton/seg7/ad_analytics/view/MainView.java
@@ -1,63 +1,34 @@
 package org.soton.seg7.ad_analytics.view;
-
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import org.soton.seg7.ad_analytics.controller.OverviewController;
-import org.soton.seg7.ad_analytics.model.DBHandler;
-import org.soton.seg7.ad_analytics.model.DBQuery;
-import org.soton.seg7.ad_analytics.model.Parser;
-import org.soton.seg7.ad_analytics.model.exceptions.MongoAuthException;
-
 public class MainView extends Application {
-
     private Stage primaryStage;
     private BorderPane rootLayout;
 
-    private ObservableList<Graph> graphData = FXCollections.observableArrayList();
-
     public MainView() {
-        
-    	graphData.add(new LineGraph("Click-through-rate"));
-    	graphData.add(new LineGraph("Cost-per-click"));
-    	graphData.add(new LineGraph("Number of Clicks"));
-    	graphData.add(new LineGraph("Number of Conversions"));
-        graphData.add(new LineGraph("Number of Impressions"));
-        graphData.add(new LineGraph("Total Cost"));
-    }
 
-    public ObservableList<Graph> getGraphData() {
-        return graphData;
     }
-
     @Override
     public void start(Stage primaryStage) {
-    	this.loadTestData();
         this.primaryStage = primaryStage;
-
         initRootLayout();
-
         showOverview();
     }
-
     public void initRootLayout() {
         try {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainView.class.getResource("RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
-
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
@@ -66,68 +37,25 @@ public class MainView extends Application {
             e.printStackTrace();
         }
     }
-
-   
     public void showOverview() {
         try {
             // Load overview.
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainView.class.getResource("Overview.fxml"));
             AnchorPane overview = (AnchorPane) loader.load();
-            
-         // Give the controller access to the main app.
+            // Give the controller access to the main app.
             OverviewController controller = loader.getController();
             controller.setMainView(this);
-
             // Set overview into the center of root layout.
             rootLayout.setCenter(overview);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-   
     public Stage getPrimaryStage() {
         return primaryStage;
     }
-
     public static void main(String[] args) {
         launch(args);
-    }
-    
-    
-    //function that loads data to database and gets clicks data, and prints the data with description
-    // format of printed data: Entry with name: 2015-01-01 has key: 12  and value: 94
-    public void loadTestData(){
-    	File clickFile = new File(new File("").getAbsolutePath().toString() + "/static/analytics_csv/click_log.csv");
-        File impressionsFile = new File(new File("").getAbsolutePath().toString() + "/static/analytics_csv/impression_log.csv");
-        File serverFile = new File(new File("").getAbsolutePath().toString() + "/static/analytics_csv/server_log.csv");
-
-    	DBHandler handler;
-    	try{
-    		//loading data
-            handler = DBHandler.getDBConnection();
-            handler.dropCollection("impression_log");            
-            handler.dropCollection("server_log");
-            handler.dropCollection("click_log");
-            Parser.parseCSV(impressionsFile);
-            Parser.parseCSV(serverFile);
-            Parser.parseCSV(clickFile);
-            
-            //getting data from database
-            Map<String, Map<String, Integer>> clickCount = DBQuery.getNumClicks();
-            
-            //printing data
-            for (Map.Entry<String, Map<String, Integer>> entryMap : clickCount.entrySet()){
-            	String name = entryMap.getKey();
-            	Map<String, Integer> data = entryMap.getValue();
-            	for(Map.Entry<String, Integer> entry : data.entrySet()){
-            		System.out.println("Entry with name: "+name+" has key: "+entry.getKey()+ "  and value: " + entry.getValue());
-            	}
-            }
-
-        } catch (MongoAuthException e) {
-            e.printStackTrace();
-        }
     }
 }
