c17126ac6afde6d00a8637dee2280cdfe45cb25f
Kanter666 <ak2g15@soton.ac.uk>
2017-03-01 03:15:13 +0000

    Changed format of variables, added function for returning files

--- src/main/java/org/soton/seg7/ad_analytics/controller/FileLoaderController.java
+++ src/main/java/org/soton/seg7/ad_analytics/controller/FileLoaderController.java
@@ -4,12 +4,27 @@ import java.io.File;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+//controller of file loader
+//TODO pass overview controller and pass chosen files in handleSartLoadingButtonAction() function
+//TODO make tests if user chose right files(correct format)
 public class FileLoaderController {
 	
 	private Stage stage;
+	//variables for labels
+	@FXML
+	private Label ClickLogT;
+	@FXML
+	private Label ServerLogT;
+	@FXML
+	private Label ImpressionLogT;
+	
+	File clickLog;
+	File serverLog;
+	File impressionLog;
 	
 	public void init(Stage primaryStage){
 		this.stage = stage;
@@ -17,28 +32,33 @@ public class FileLoaderController {
 	
 	//function that handles pressing of Click Log button
     @FXML protected void handleLoadClickButtonAction(ActionEvent event) {
-        System.out.println("click");
-        File clickLog = fileChooser("Choose click log file: ");
+        clickLog = fileChooser("Choose click log file: ");
+        ClickLogT.setText(""+clickLog);
     }
     
     //function that handles pressing of Server Log button
     @FXML protected void handleLoadServerButtonAction(ActionEvent event) {
-        System.out.println("server");
-        File serverLog = fileChooser("Choose server log file: ");
-
+        serverLog = fileChooser("Choose server log file: ");
+        ServerLogT.setText(""+serverLog);
     }
     
     //function that handles pressing of Impression Log button
     @FXML protected void handleLoadImpressionButtonAction(ActionEvent event) {
-        System.out.println("impression");
-        File impressionLog = fileChooser("Choose impression log file: ");
-
+        impressionLog = fileChooser("Choose impression log file: ");
+        ImpressionLogT.setText(""+impressionLog);
     }
     
+    //TODO pass values to overview controller
+    @FXML protected void handleSartLoadingButtonAction(ActionEvent event){
+    	
+    }
+    
+    //runs file chooser
+    //takes title of file explorer window as argument
     public File fileChooser(String title){
     	
     	FileChooser fileChooser = new FileChooser();
-    	fileChooser.setTitle("Open Resource File");
+    	fileChooser.setTitle(title);
     	fileChooser.setInitialDirectory(
                 new File(System.getProperty("user.home"))
             ); 
