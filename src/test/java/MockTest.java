b31d6e76ecc060bef565947bbe2b88107da7d60d
Bogdan Buduroiu <bbuduroiu@gmail.com>
2017-04-27 15:06:30 +0100

    Pre-commit tests

---
+++ src/test/java/MockTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by bogdanbuduroiu on 27/04/2017.
+ */
+public class MockTest {
+
+    @Test
+    public void testTest() {
+        Assert.assertTrue(true);
+    }
+}
